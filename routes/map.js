ed25b7fd0558a06f6b49ca5a8c9f97c41a63feb2
Aakash Gandhi <agandhi4@gmail.com>
2016-08-13 14:15:25 -0400

    Working map :D

--- routes/map.js
+++ routes/map.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var api_keys = require('../api_keys')
 
 router.get('/', function(req, res, next) {
-    res.render('map', { title: 'Map' });
+  res.render('map', {api_key: api_keys['google_maps']});
 });
 
 module.exports = router;
